72a0188101ad6393151657aadf316428908205e5
Yunus Emre Baş <emrebasyunus1@gmail.com>
2023-06-11 21:19:42 +0300

    Yunus Emre
    .

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1. Sayı : ");
+
+        a = input.nextInt();
+
+        System.out.println("2. Sayı : ");
+
+        b = input.nextInt();
+
+        System.out.println("3. Sayı : ");
+
+        c = input.nextInt();
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.println("a < b < C");
+            } else {
+                System.out.println("a < c < b");
+
+            }
+        } else if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.println("b < a < c ");
+            } else {
+                System.out.println("b < c < a");
+            }
+        } else if ((c < b) && (c < a)) {
+            if (a < b) {
+                System.out.println("c < a < b");
+            } else {
+                System.out.println("c < b < a");
+            }
+
+
+        }
+
+    }
+
+}
